b0684196c1b6afc289766470c35ed1fd333ef9ea
stenoritama <shr.nrht.0623@gmail.com>
2017-05-08 19:51:23 +0900

    singleton

---
+++ src/singleton/Singleton.java
@@ -0,0 +1,15 @@
+package singleton;
+
+public class Singleton {
+
+    private static Singleton singleton = new Singleton();
+
+    private Singleton() {
+        System.out.println("インスタンスを作成しました。");
+    }
+
+    public static Singleton getInstance() {
+        return singleton;
+    }
+
+}
